24182f37b875f24d5f2689ba87a707f683b7cc6f
laike <924462390@qq.com>
2020-02-15 21:22:05 +0800

    new code

--- android/app/src/main/java/com/rncnode/MainApplication.java
+++ android/app/src/main/java/com/rncnode/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rncnode;
+package com.rngithub;
 
 import android.app.Application;
 import android.content.Context;
--- ios/rngithubTests/rngithubTests.m
+++ ios/rngithubTests/rngithubTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rncnodeTests : XCTestCase
+@interface rngithubTests : XCTestCase
 
 @end
 
-@implementation rncnodeTests
+@implementation rngithubTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
