4ca19b33ebe358a0ce2f3981cbd98b48aca65c92
laike <924462390@qq.com>
2020-02-20 11:54:42 +0800

    performance the android app  deep think mode

--- src/container/Setting.js
+++ src/container/Setting.js
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import WebView from 'react-native-webview';
 export default class Setting extends Component {
   render() {
     return (
-      <View style={styles}>
-        <Text>this is Setting Page </Text>
-      </View>
+      <WebView
+        style={styles.container}
+        source={{uri: 'https://baidu.com/'}}
+        startInLoadingState={true}></WebView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
+    flex: 1,
     backgroundColor: 'red',
   },
 });
